bc5b2826cf154c16943acc3f85d492b989c71649
Alyssa Cabello <48364178+Hollenfeuer1@users.noreply.github.com>
2021-08-26 15:59:30 -0500

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    let url = location.href.replace(/\/$/, "");
+   
+    if (location.hash) {
+      const hash = url.split("#");
+      $('#myTab a[href="#'+hash[1]+'"]').tab("show");
+      url = location.href.replace(/\/#/, "#");
+      history.replaceState(null, null, url);
+      setTimeout(() => {
+        $(window).scrollTop(0);
+      }, 400);
+    } 
+     
+    $('a[data-toggle="tab"]').on("click", function() {
+      let newUrl;
+      const hash = $(this).attr("href");
+      if(hash == "#home") {
+        newUrl = url.split("#")[0];
+      } else {
+        newUrl = url.split("#")[0] + hash;
+      }
+      newUrl += "/";
+      history.replaceState(null, null, newUrl);
+    });
+  });
+
+  $(document).ready(function () {
+    $(".outsidelink").click(function () {
+        $('.nav a[href="' +  $(this).attr("href") + '"]').tab('show');          
+    });
+});
